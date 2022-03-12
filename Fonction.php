28481fd52da8a7f03b48501b3b7500e3472dfce2
Rakotondramanana Ranto <77172572+RantoPenjy@users.noreply.github.com>
2022-03-12 16:52:05 +0300

    Update Fonction.php

--- Fonction.php
+++ Fonction.php
@@ -1,51 +1,59 @@
 <?php
 
-class Fonction{
-
+class Fonction
+{
     public $delta, $x1, $x2;
 
-    public function __construct(int $a, int $b, int $c){
+    public function __construct(int $a, int $b, int $c)
+    {
         $this->a = $a;
         $this->b = $b;
         $this->c = $c;
     }
 
-    public function delta(){
+    public function delta()
+    {
         $this->delta = pow($this->b,2) - 4 * $this->a * $this->c ;
+        
         return $this->delta;
     }
 
-    public function x1(){
+    public function x1()
+    {
         $this->x1 = (- $this->b + sqrt($this->delta)) / 2 * $this->a ;
+        
         return $this->x1;
     }
 
-    public function x2(){
+    public function x2()
+    {
         $this->x2 = (- $this->b - sqrt($this->delta)) / 2 * $this->a;
+        
         return $this->x2;
     }
 
-    public function x(){
+    public function x()
+    {
         $this->x = - $this->b / 2 * $this->a ;
+        
         return $this->x;
     }
 
-    public function calculx(){
-        if ($this->delta < 0) {
-
+    public function calculx()
+    {
+        if ($this->delta < 0)
+        {
             echo "Pas de solution";
-
-        }elseif ($this->delta == 0) {
-
+        }
+        elseif ($this->delta == 0)
+        {
             echo "x = ". $this->x();
-
-        }else{
+        }
+        else
+        {
             echo "x1 = ". $this->x1() ;
             echo "<br>";
             echo "x2 = ". $this->x2() ;
         }
     }
-
 }
-
- ?>
